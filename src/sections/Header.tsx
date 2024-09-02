9e9bb9fb53673d5990f1ad0e069d9e95e8a407ee
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2024-09-02 17:38:07 +0530

    first commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,28 @@
+import { CutCornerButton } from "../components/CutCornerButton";
+
+export const HeaderSection = () => {
+  return (
+    <>
+      <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg z-50">
+        <div className="container">
+          <div className="flex justify-between items-center h-24 md:h-28">
+            <div className="">
+              <img src="/assets/images/logo.svg" alt="blockforgelogo" />
+            </div>
+            <div className="flex gap-4 items-center">
+              <CutCornerButton className="hidden md:inline-flex">Get Started</CutCornerButton>
+              <div className="size-10 relative">
+                <div className="absolute left-1/2 top-1/2 -translate-y-1/2">
+                  <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+                </div>
+                <div className="absolute left-1/2 top-1/2 -translate-y-1/2">
+                  <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
