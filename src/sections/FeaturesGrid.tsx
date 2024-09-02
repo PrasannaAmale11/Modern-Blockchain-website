78a9ada62290efc21d88b0c8bea36ae4907eee20
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2024-09-02 22:33:03 +0530

    added new section with new content

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -1,54 +1,66 @@
-import { CutCornerButton } from "../components/CutCornerButton"
-import { LearnMoreButtonComponent } from "../components/LearnMoreBtn"
+import { CutCornerButton } from "../components/CutCornerButton";
+import { LearnMoreButtonComponent } from "../components/LearnMoreBtn";
 
 const listItems = [
-    'Experience unparalleled security and scalability',
-    'Fully benifit from scalable network effects',
-    'Unlock the potential of decentralized networks'
-]
+  "Experience unparalleled security and scalability",
+  "Fully benifit from scalable network effects",
+  "Unlock the potential of decentralized networks",
+];
 
 export const FeaturesGridSection = () => {
-    return <>
-    <section className="py-24">
+  return (
+    <>
+      <section className="py-24">
         <div className="container">
-            <div>
-            <h2 className="font-heading font-black text-4xl">Empowering the future of blockchain.</h2>
+            <div className="flex flex-col gap-56">
+          <div>
+            <h2 className="font-heading font-black text-4xl">
+              Empowering the future of blockchain.
+            </h2>
             <p className="text-xl text-zinc-400 mt-8">
-                Blockforge provides robust and secure infrastructure to support the next generation of decentralized applications.
+              Blockforge provides robust and secure infrastructure to support
+              the next generation of decentralized applications.
             </p>
             <ul className="flex flex-col gap-8 mt-12">
-                {listItems.map((item) => (
-                    <li key={item} className="flex items-center gap-3">
-                        <div className="inline-flex justify-center items-center size-8 outline outline-4 -outline-offset-4 rounded-full outline-fuchsia-500/10 flex-shrink-0">
-                            <div className="size-1.5 bg-fuchsia-500 rounded-full">
-                            </div>
-                        </div>
-                        <span className="text-xl font-bold">{item}</span>
-                    </li>
-                ))}
+              {listItems.map((item) => (
+                <li key={item} className="flex items-center gap-3">
+                  <div className="inline-flex justify-center items-center size-8 outline outline-4 -outline-offset-4 rounded-full outline-fuchsia-500/10 flex-shrink-0">
+                    <div className="size-1.5 bg-fuchsia-500 rounded-full"></div>
+                  </div>
+                  <span className="text-xl font-bold">{item}</span>
+                </li>
+              ))}
             </ul>
-        </div>
-        <div className=" flex gap-8 mt-12">
-            <CutCornerButton>
-                Get Started
-            </CutCornerButton>
-           <LearnMoreButtonComponent>
-            Learn More
-           </LearnMoreButtonComponent>
-        </div>
 
-        <div className="">
-            <h2>Blockforge leads the way.</h2>
-            <p>Blockforge is dedicated to supporting the evolution of web3 applications by delivering the necessary infrastructure and security for web3.</p>
-            <p>Blockforge champtions web3 for everyone. As a decentralized blockchain scaling platform, Blockforge enables developers to create scalable, user-friendly apps with low transcation costs, all while ensuing robust security.</p>
-            <div className="flex">
-                <CutCornerButton>Get Started</CutCornerButton>
-                <LearnMoreButtonComponent>Learn More</LearnMoreButtonComponent>
+            <div className=" flex gap-8 mt-12">
+              <CutCornerButton>Get Started</CutCornerButton>
+              <LearnMoreButtonComponent>Learn More</LearnMoreButtonComponent>
             </div>
-        </div>
+          </div>
 
+          <div className="">
+            <h2 className="font-heading font-black text-4xl">Blockforge leads the way.</h2>
+            <div className="flex flex-col text-xl text-zinc-400 mt-6 gap-6">
+            <p>
+              Blockforge is dedicated to supporting the evolution of web3
+              applications by delivering the necessary infrastructure and
+              security for web3.
+            </p>
+            <p>
+              Blockforge champions web3 for everyone. As a decentralized
+              blockchain scaling platform, Blockforge enables developers to
+              create scalable, user-friendly apps with low transcation costs,
+              all while ensuing robust security.
+            </p>
+            </div>
+            <div className="flex gap-8 mt-12">
+              <CutCornerButton>Get Started</CutCornerButton>
+              <LearnMoreButtonComponent>Learn More</LearnMoreButtonComponent>
+            </div>
+          </div>
+          </div>
         </div>
-    </section>
-    
+      </section>
     </>
-}
+  );
+};
