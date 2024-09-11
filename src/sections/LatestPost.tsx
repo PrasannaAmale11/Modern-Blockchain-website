f840a1b17ab514e2505e0117e5b693545a0d6cb0
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2024-09-11 15:34:39 +0530

    Added small marginTop animation to Latestpost section using framer-motion

--- src/sections/LatestPost.tsx
+++ src/sections/LatestPost.tsx
@@ -4,11 +4,20 @@ import { getPostColorsFromCategory } from "../utils/postUtils";
 import { Tag } from "../components/Tag";
 import { CutCornerButton } from "../components/CutCornerButton";
 import { twMerge } from "tailwind-merge";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const LastestPostSection = (props: {
   latestPosts: CollectionEntry<"blog">[];
 }) => {
   const { latestPosts } = props;
+
+  const targetRef = useRef(null);
+ const {scrollYProgress}=  useScroll({
+    target: targetRef,
+    offset:['start end','start center']
+  })
+  const marginTop = useTransform(scrollYProgress, [0,1],[0,64])
   return (
     <>
       <section className="py-60">
@@ -43,7 +52,9 @@ export const LastestPostSection = (props: {
               )
             )}
             </div>
-            <div className="hidden md:flex flex-col gap-8 mt-16">
+            <motion.div className="hidden md:flex flex-col gap-8 mt-16" ref={targetRef} style={{
+              marginTop: marginTop,
+            }}>
             {latestPosts.map(
               ({ data: { title, category, description } }, postIndex) => (
                 <CardComponent
@@ -62,7 +73,7 @@ export const LastestPostSection = (props: {
                 </CardComponent>
               )
             )}
-            </div>
+            </motion.div>
           </div>
           <div className=" flex justify-center mt-48 md:mt-32">
             <CutCornerButton>
