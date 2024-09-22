c33e49656e574df2de9718d7669c2d1176098d0c
PrasannaAmale11 <amaleprasanna2001@gmail.com>
2024-09-22 18:54:38 +0530

    Added navigation menu with links and animations

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
 import { CutCornerButton } from "../components/CutCornerButton";
+import { HexagonSvgComponent } from "../components/HexagonSvg";
+import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
+const navLinks = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Blog",
+    link: "/blog",
+  },
+  {
+    title: "Careers",
+    link: "/careers",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+];
 
 export const HeaderSection = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(()=>{
+    if (isOpen) {
+      document.body.style.overflow = "hidden"
+    }else{
+       document.body.style.overflow = "auto"
+    }
+  },[isOpen])
   return (
     <>
       <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg z-50">
@@ -10,19 +40,77 @@ export const HeaderSection = () => {
               <img src="/assets/images/logo.svg" alt="blockforgelogo" />
             </div>
             <div className="flex gap-4 items-center">
-              <CutCornerButton className="hidden md:inline-flex">Get Started</CutCornerButton>
-              <div className="size-10 relative">
+              <CutCornerButton className="hidden md:inline-flex">
+                Get Started
+              </CutCornerButton>
+              <div
+                className="size-10 relative cursor-pointer"
+                onClick={() => setIsOpen((curr) => !curr)}
+              >
                 <div className="absolute left-1/2 top-1/2 -translate-y-1/2">
-                  <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+                  <div
+                    className={twMerge(
+                      "w-5 h-0.5 bg-zinc-300 -translate-y-1 transition-all duration-500",
+                      isOpen && "rotate-45 translate-y-0"
+                    )}
+                  ></div>
                 </div>
                 <div className="absolute left-1/2 top-1/2 -translate-y-1/2">
-                  <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+                  <div
+                    className={twMerge(
+                      "w-5 h-0.5 bg-zinc-300 translate-y-1 transition-all duration-500",
+                      isOpen && "-rotate-45 translate-y-0"
+                    )}
+                  ></div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </header>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+          initial={{
+            opacity:0,
+          }}
+          animate={{opacity:1}}
+          exit={{opacity:0}}
+          transition={{
+            duration:0.5
+          }}
+          className="fixed size-full top-0 left-0 z-30 bg-zinc-900 ">
+            <div className="absolute inset-2 rounded-md bg-zinc-800 mt-24 z-0">
+              <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
+                <HexagonSvgComponent size={700} />
+              </div>
+              <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
+                <HexagonSvgComponent size={1100} />
+              </div>
+              <div className="flex h-full justify-center items-center">
+                <nav className="flex flex-col items-center gap-12">
+                  {navLinks.map((item, index) => (
+                    <motion.a 
+                    initial = {{opacity:0, y:20}}
+                    animate={{opacity:1, y:0}}
+                    transition={{
+                      duration:0.5,
+                      ease: "linear",
+                      delay: 0.25 * index
+                    }}
+                      href={item.link}
+                      key={index}
+                      
+                    >
+                     <span className="text-4xl font-heading font-black text-zinc-500 hover:text-zinc-300 transition duration-300">{item.title}</span>
+                    </motion.a>
+                  ))}
+                </nav>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
